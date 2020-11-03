95bf29188355f0043d6faf11106fab063b8300f5
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2020-11-03 10:31:57 +0100

    feat(): export inherit prop initializers

--- lib/index.ts
+++ lib/index.ts
@@ -1,10 +1,11 @@
-export * from './mapped-type.interface';
 export * from './intersection-type.helper';
+export * from './mapped-type.interface';
 export * from './omit-type.helper';
 export * from './partial-type.helper';
 export * from './pick-type.helper';
 export {
   applyIsOptionalDecorator,
+  inheritPropertyInitializers,
   inheritTransformationMetadata,
   inheritValidationMetadata,
 } from './type-helpers.utils';
