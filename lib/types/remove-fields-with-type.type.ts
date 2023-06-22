ad2293fe84ed7d52eb350bd75e4da2b899b93ec3
Micael Levi L. Cavalcante <mllc@icomp.ufam.edu.br>
2023-06-21 20:54:10 -0400

    fix(types): `RemoveFieldsWithType` when excluding optional fields
    without strict null checks

--- lib/types/remove-fields-with-type.type.ts
+++ lib/types/remove-fields-with-type.type.ts
@@ -1,23 +1,5 @@
-// With this util we could remove the keys with never type from the original type.
-type KeysWithoutType<T, Type> = {
-  [K in keyof T]: T[K] extends Type ? never : K;
+type KeysWithType<T, Type> = {
+  [K in keyof T]: T[K] extends Type ? K : never;
 }[keyof T];
 
-type KeysOfType<T, U> = { [K in keyof T]: T[K] extends U ? K : never }[keyof T];
-type RequiredKeys<T> = Exclude<
-  KeysOfType<T, Exclude<T[keyof T], undefined>>,
-  undefined
->;
-type OptionalKeys<T> = Exclude<keyof T, RequiredKeys<T>>;
-
-export type RemoveFieldsWithType<T, Type> = {
-  [K in KeysWithoutType<Pick<T, RequiredKeys<T>>, Type>]: Pick<
-    T,
-    RequiredKeys<T>
-  >[K];
-} & {
-  [K in KeysWithoutType<Pick<T, OptionalKeys<T>>, Type>]?: Pick<
-    T,
-    OptionalKeys<T>
-  >[K];
-};
+export type RemoveFieldsWithType<T, Type> = Exclude<T, KeysWithType<T, Type>>;
